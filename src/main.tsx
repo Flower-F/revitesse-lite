ede7d0f57ea41f6018f45f67e6c87d51d502f6c3
FlowerF <1647231392@qq.com>
2023-05-06 11:54:04 +0800

    chore: update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import '@unocss/reset/tailwind.css';
 import 'uno.css';
 import './styles/main.css';
 
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
 );
