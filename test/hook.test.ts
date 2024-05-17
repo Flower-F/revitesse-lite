71388a11b10ff2ab6bee95f1c6630cc934c846f7
FlowerF <1647231392@qq.com>
2024-05-17 22:43:23 +0800

    feat: remove act function in hook testing

--- test/hook.test.ts
+++ test/hook.test.ts
@@ -1,14 +1,16 @@
-import { act, renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { useCounter } from 'ahooks';
 import { describe, expect, it } from 'vitest';
 
 describe('useCounter', () => {
   it('increment & decrement', () => {
-    const { result } = renderHook(() => useCounter());
+    const { result, rerender } = renderHook(() => useCounter());
     expect(result.current[0]).toMatchInlineSnapshot('0');
-    act(() => result.current[1].inc(1));
+    result.current[1].inc(1);
+    rerender();
     expect(result.current[0]).toMatchInlineSnapshot('1');
-    act(() => result.current[1].dec(1));
+    result.current[1].dec(1);
+    rerender();
     expect(result.current[0]).toMatchInlineSnapshot('0');
   });
 });
